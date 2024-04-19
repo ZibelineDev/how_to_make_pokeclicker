1e529f9050b1a1836fffe5667643ef6c1a2ae6c9
Zibeline <zibeline.dev@gmail.com>
2024-04-19 19:56:14 +0200

    📝 Update prototype documentation

--- scenes/prototypes/main_view/main_view.gd
+++ scenes/prototypes/main_view/main_view.gd
@@ -1,19 +1,23 @@
 class_name ProtoMainView
 extends VBoxContainer
+## A prototype meant to replicate a rudimentary clicker feature.
 
 
+## Reference to the ProgressBar displaying Hp
 @export
 var progress_bar : ProgressBar
-
+## Data object
 var data : Data = Data.new()
-
+## Current HP of the wild Pokémon
 var hp : float = 10
 
 
+## Ready method
 func _ready() -> void:
 	progress_bar.value = hp
 
 
+## Reduce the HP by 1 and checks for reset
 func attack() -> void:
 	hp -= 1
 	progress_bar.value = hp
@@ -22,6 +26,7 @@ func attack() -> void:
 		reset()
 
 
+## Reset the HP back to 10 & grant pokédollars
 func reset() -> void:
 	hp = 10
 	progress_bar.value = hp
@@ -30,5 +35,6 @@ func reset() -> void:
 	print("Pokédollar : %s" %data.pokedollar)
 
 
+## Triggered when the TextureButton is pressed
 func _on_texture_button_pressed() -> void:
 	attack()
