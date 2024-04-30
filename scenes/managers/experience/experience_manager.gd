b56dcd8a830d8aca1e3e2aeb9c41daad869e0446
Zibeline <zibeline.dev@gmail.com>
2024-05-01 01:49:00 +0200

    ✨ Add a level formula

--- scenes/managers/experience/experience_manager.gd
+++ scenes/managers/experience/experience_manager.gd
@@ -20,16 +20,7 @@ func update_level(id : String) -> void:
 	var level : int = 1
 	var should_notify : bool = false
 	
-	if experience >= 10:
-		level = 2
-	if experience >= 25:
-		level = 3
-	if experience >= 75:
-		level = 4
-	if experience >= 150:
-		level = 5
-	if experience >= 250:
-		level = 6
+	level = experience ** (1.0/3.0)
 	
 	@warning_ignore("unsafe_cast")
 	var old_level : int = (Game.ref.data.captured_pokemons[id] as DataCapturedPokemon).level
